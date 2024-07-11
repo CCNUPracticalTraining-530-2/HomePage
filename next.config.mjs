865666c5b5ce2d369aafe85a06be0b1c64464911
墨綠BG <s5460703@gmail.com>
2024-07-12 05:38:48 +0800

    🔧 chore：配置 Next.js 设定档

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+import { withSentryConfig } from '@sentry/nextjs';
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withSentryConfig(
+  nextConfig,
+  {
+    silent: true,
+    org: 'javascript-mastery',
+    project: 'javascript-nextjs',
+  },
+  {
+    widenClientFileUpload: true,
+    transpileClientSDK: true,
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
+  }
+);
