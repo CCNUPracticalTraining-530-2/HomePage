1f15516b4bfc0b1188ff2204ba9f2ff23033f035
墨綠BG <s5460703@gmail.com>
2024-07-13 00:04:20 +0800

    🔧 chore：配置 sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,19 @@
-import * as Sentry from '@sentry/nextjs';
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: 'https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408',
+  dsn: "https://fe49d1e6e005aef891c38f24e076915e@o4507585195999232.ingest.us.sentry.io/4507589608603648",
+
+  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
 });
--- src/app/api/sentry-example-api/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export const dynamic = 'force-dynamic';
-
-export function GET() {
-  throw new Error('Sentry Example API Route Error');
-  return NextResponse.json({ data: 'Testing Sentry Error...' });
-}
